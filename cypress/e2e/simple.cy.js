370166c7c102432649435bf5f57ee38e311bbbd8
Sergei Shabanov <shabanov1989@gmail.com>
2024-08-06 19:28:11 +0300

    feat: добавил baseUrl и alias в cypress тесты

--- cypress/e2e/simple.cy.js
+++ cypress/e2e/simple.cy.js
@@ -5,7 +5,7 @@ describe('Application', function () {
     let email = '123456@test.com';
     let password = '123456';
 
-    cy.visit('http://localhost:3000/login');
+    cy.visit('login');
     cy.get('input[name="email"]').type(email);
     cy.get('input[name="password"]').type(password);
     cy.get('button[type="submit"]').click();
@@ -18,30 +18,30 @@ describe('Application', function () {
 
   it('should close modal after click close button', () => {
     cy.get('[data-testid="bun"]').first().click();
-    cy.get('[data-testid="ingredient-name"]').should('have.text', 'Ингредиент 1');
+    cy.get('[data-testid="ingredient-name"]').as('ingredient').should('have.text', 'Ингредиент 1');
     cy.get('[data-testid="modal-close-button"]').click();
-    cy.get('[data-testid="ingredient-name"]').should('not.exist');
+    cy.get('@ingredient').should('not.exist');
   });
 
   it('should close modal after click overlay', () => {
     cy.get('[data-testid="bun"]').first().click();
-    cy.get('[data-testid="ingredient-name"]').should('have.text', 'Ингредиент 1');
+    cy.get('[data-testid="ingredient-name"]').as('ingredient').should('have.text', 'Ингредиент 1');
     cy.get('[data-testid="modal-overlay"]').click('left', { force: true });
-    cy.get('[data-testid="ingredient-name"]').should('not.exist');
+    cy.get('@ingredient').should('not.exist');
   });
 
   it('should drop on drop-target', () => {
     cy.get('[data-testid="bun"]').trigger('dragstart');
-    cy.get('[data-testid="drop-target"]').trigger('drop');
-    cy.get('[data-testid="drop-target"]').should('have.text', 'Ингредиент 1 (вверх)1255');
+    cy.get('[data-testid="drop-target"]').as('dropTarget').trigger('drop');
+    cy.get('@dropTarget').should('have.text', 'Ингредиент 1 (вверх)1255');
   });
 
   it('should create order', () => {
     cy.intercept('POST', 'orders', { fixture: 'orders' });
     cy.get('[data-testid="bun"]').trigger('dragstart');
-    cy.get('[data-testid="drop-target"]').trigger('drop');
+    cy.get('[data-testid="drop-target"]').as('dropTarget').trigger('drop');
     cy.get('[data-testid="sauce"]').trigger('dragstart');
-    cy.get('[data-testid="drop-target"]').trigger('drop');
+    cy.get('@dropTarget').trigger('drop');
     cy.contains('Оформить заказ').click();
     cy.get('[data-testid="order-number"]').should('have.text', '1');
   });
